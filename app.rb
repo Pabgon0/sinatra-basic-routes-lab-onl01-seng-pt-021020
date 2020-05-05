af7ef55ddb4969b754375e4bd514b37e9ea459ec
Pablo Gonzalez <pabgon0@gmail.com>
2020-05-05 20:27:56 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Pablo"
+  end
+  
+  get '/hometown' do
+    "My hometown is Trussville"
+  end
+  
+  get '/favorite-song' do
+    "My favorite song is Tears Dont Fall"
+  end
 end
